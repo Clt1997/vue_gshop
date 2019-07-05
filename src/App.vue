1f3934acb6016d342559f3ac465efdffcee03591
Clt-97 <email@example.com>
2019-07-05 15:19:27 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide></FooterGuide>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import FooterGuide from "./components/FooterGuide/FooterGuide";
+export default {
+  components: {
+    FooterGuide
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+// @import '../../common/stylus/mixins.styl'
+#app
+  width 100%
+  height 100%
+  background #f5f5f5
+  position relative
+</style>
---
+++ src/router/routers.js
@@ -0,0 +1,21 @@
+import Msite from '../pages/Msite/Msite.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Search from '../pages/Search/Search.vue'
+
+export default [{
+  path: '/',
+  redirect: './msite'
+}, {
+  path: '/msite',
+  component: Msite
+}, {
+  path: '/search',
+  component: Search
+}, {
+  path: '/order',
+  component: Order
+}, {
+  path: '/profile',
+  component: Profile
+}]
