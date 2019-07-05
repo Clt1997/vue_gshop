333f40c9c003c7f787adf49b03a82760c6fc6141
Clt-97 <email@example.com>
2019-07-05 15:44:12 +0800

    change some

--- src/router/routers.js
+++ src/router/routers.js
@@ -2,20 +2,36 @@ import Msite from '../pages/Msite/Msite.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import Search from '../pages/Search/Search.vue'
+import Login from '../pages/Login/Login.vue'
 
-export default [{
-  path: '/',
-  redirect: './msite'
-}, {
-  path: '/msite',
-  component: Msite
-}, {
-  path: '/search',
-  component: Search
-}, {
-  path: '/order',
-  component: Order
-}, {
-  path: '/profile',
-  component: Profile
-}]
+export default [
+
+  {
+    path: '/',
+    redirect: './msite'
+  },
+  {
+    path: '/msite',
+    component: Msite
+  },
+  {
+    path: '/search',
+    component: Search
+  },
+  {
+    path: '/order',
+    component: Order
+  },
+  {
+    path: '/login',
+    component: Login,
+    meta: {
+      xxx: 'abc'
+    }
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+
+]
