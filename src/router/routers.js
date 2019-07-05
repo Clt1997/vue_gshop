43a449c80cb66cb0ed19ece3c0dc1001d91c6c8c
Clt-97 <email@example.com>
2019-07-05 15:49:10 +0800

    15:49

--- src/router/routers.js
+++ src/router/routers.js
@@ -2,6 +2,7 @@ import Msite from '../pages/Msite/Msite.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import Search from '../pages/Search/Search.vue'
+import Login from '../pages/Login/Login.vue'
 
 export default [{
   path: '/',
@@ -18,4 +19,7 @@ export default [{
 }, {
   path: '/profile',
   component: Profile
+}, {
+  path: '/login',
+  component: Login
 }]
