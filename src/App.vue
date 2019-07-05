e94ba50d894a48aed5be22ca8e1656b14905b399
Clt-97 <email@example.com>
2019-07-05 16:01:08 +0800

    00000

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <FooterGuide></FooterGuide>
+    <FooterGuide v-if="$route.meta.showfoot"></FooterGuide>
   </div>
 </template>
 
--- src/router/routers.js
+++ src/router/routers.js
@@ -4,22 +4,42 @@ import Profile from '../pages/Profile/Profile.vue'
 import Search from '../pages/Search/Search.vue'
 import Login from '../pages/Login/Login.vue'
 
-export default [{
-  path: '/',
-  redirect: './msite'
-}, {
-  path: '/msite',
-  component: Msite
-}, {
-  path: '/search',
-  component: Search
-}, {
-  path: '/order',
-  component: Order
-}, {
-  path: '/profile',
-  component: Profile
-}, {
-  path: '/login',
-  component: Login
-}]
+export default [
+
+  {
+    path: '/',
+    redirect: './msite'
+  },
+  {
+    path: '/msite',
+    component: Msite,
+    meta: {
+      showfoot: true
+    }
+  },
+  {
+    path: '/search',
+    component: Search,
+    meta: {
+      showfoot: true
+    }
+  },
+  {
+    path: '/order',
+    component: Order,
+    meta: {
+      showfoot: true
+    }
+  },
+  {
+    path: '/profile',
+    component: Profile,
+    meta: {
+      showfoot: true
+    }
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
