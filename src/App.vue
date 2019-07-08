0fa6ecd6f4a57c7ae79f16d1c19e93b222d79f0d
Clt-97 <email@example.com>
2019-07-08 16:40:00 +0800

    mmmmm

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
-    <router-view></router-view>
-    <FooterGuide v-if="$route.meta.showfoot"></FooterGuide>
+    <router-view />
+    <FooterGuide v-if="$route.meta.isShowFooter"/>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import FooterGuide from "./components/FooterGuide/FooterGuide";
-export default {
-  components: {
-    FooterGuide
+  import FooterGuide from './components/FooterGuide/FooterGuide'
+  export default {
+    name: "App",
+    data () {
+      return {
+        msg: 'App Component'
+      }
+    },
+
+    async mounted () {
+      // 分发actoin, 从后台获取数据到state中
+      this.$store.dispatch('getAddress')
+    },
+
+    components: {
+      FooterGuide
+    }
   }
-};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-// @import '../../common/stylus/mixins.styl'
-#app
-  width 100%
-  height 100%
-  background #f5f5f5
-  position relative
 </style>
